b0c5c8bb9cc2f1e9162589dda64f60f6ff5faa1b
JerryDdie77 <shahterchannel@gmail.com>
2025-12-15 10:21:57 +0300

    Добавлена функция для вывода имени пользователя по ID

--- Main_Logic/core/users.go
+++ Main_Logic/core/users.go
@@ -14,17 +14,17 @@ type User struct {
 }
 
 func GetUsers() ([]byte, error) {
-	// Подключаемся к БД
+
 	db, err := db.ConnectDB()
 	if err != nil {
-		return nil, fmt.Errorf("ConnectDB: ошибка подключения к БД: %v", err)
+		return nil, fmt.Errorf("ConnectDB: ошибка подключения к БД: %w", err)
 	}
 	defer db.Close()
 
 	// Выполняем запрос
 	rows, err := db.Query("SELECT id, full_name FROM users")
 	if err != nil {
-		return nil, fmt.Errorf("Query: ошибка выполнения запроса: %v", err)
+		return nil, fmt.Errorf("Query: ошибка выполнения запроса: %w", err)
 	}
 	defer rows.Close()
 
@@ -33,21 +33,41 @@ func GetUsers() ([]byte, error) {
 	for rows.Next() {
 		var user User
 		if err := rows.Scan(&user.ID, &user.FullName); err != nil {
-			return nil, fmt.Errorf("ошибка чтения строки: %v", err)
+			return nil, fmt.Errorf("rows.Scan: ошибка чтения строки: %w", err)
 		}
 		users = append(users, user)
 	}
 
 	// Проверка ошибок после итерации
 	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("ошибка при обработке результата: %v", err)
+		return nil, fmt.Errorf("rows.Err: ошибка при обработке результата: %w", err)
 	}
 
 	// Преобразуем в JSON
 	jsonData, err := json.Marshal(users)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка преобразования в JSON: %v", err)
+		return nil, fmt.Errorf("Marshal: ошибка преобразования в JSON: %w", err)
 	}
 
 	return jsonData, nil
+}
+
+func GetUserById(ID int) (string, error) {
+	db, err := db.ConnectDB()
+	if err != nil {
+		return "", fmt.Errorf("ConnectDB: ошибка подключения к БД: %w", err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query(fmt.Sprintf("SELECT full_name FROM users WHERE id = %d", ID))
+	if err != nil {
+		return "", fmt.Errorf("Query: ошибка при получении строк")
+	}
+	rows.Next()
+	var answer string
+	if err := rows.Scan(&answer); err != nil {
+		return "", fmt.Errorf("rows.Scan: ошибка чтения строки: %w", err)
+	}
+	return answer, nil
+
 }
