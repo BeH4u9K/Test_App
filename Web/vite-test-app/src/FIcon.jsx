da24ca1ee6be1467c5ffd673d1539b81fad8a2ea
BeHuk <denis-nejivl9k@yandex.ru>
2026-01-04 15:51:18 +0300

    Очистка базового проекта от шаблона Vite и React, добавление библиотек react-router-dom и MUI System, создание первого тестового окна для проверки работы React Router.

---
+++ Web/vite-test-app/src/FIcon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { styled } from '@mui/system';
+
+
+const Test = styled('div')({
+  color:'gold',
+})
+
+function FIcon() {
+  return (
+    <Test>Привет Мир</Test>
+  )
+}
+
+export default FIcon
