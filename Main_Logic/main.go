720a1e0d82e71613bc47d67ae36ae00bf75f2a18
JerryDdie77 <shahterchannel@gmail.com>
2026-01-05 12:54:10 +0300

    Использование глобальной переменной DB и одноразовая инициализация в main().

--- Main_Logic/main.go
+++ Main_Logic/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
 
+	_ "github.com/lib/pq"
+
+	"main_logic/storage"
+)
+
+func main() {
+	dsn := os.Getenv("DATABASE_URL")
+
+	var err error
+	storage.DB, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("open db: %v", err)
+	}
+
+	if err = storage.DB.Ping(); err != nil {
+		log.Fatalf("ping db: %v", err)
+	}
+	defer storage.DB.Close()
+
+	http.ListenAndServe(":8080", nil)
 }
