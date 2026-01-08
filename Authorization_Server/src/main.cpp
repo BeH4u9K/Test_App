764e9a59257df9c8c57027d2a60b179778e329e9
ElenaYarosh <yarosh.elena.a@gmail.com>
2026-01-08 16:20:42 +0300

    базовая структура Authorization Server

---
+++ Authorization_Server/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Authorization Server starting..." << std::endl;
+    
+    //инициализация конфигурации, логгера, сервера
+    
+    std::cout << "Authorization Server stopped." << std::endl;
+    return EXIT_SUCCESS;
+}
